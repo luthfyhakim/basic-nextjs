74a247199207092a26cc2c91b8fdd858b3c54ce6
luthfyhakim <luthfyhakim250404@gmail.com>
2022-05-21 11:20:03 +0700

    menampilkan halaman detail user dg fetching api

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,17 +1,65 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
 
-export default function DetailUser() {
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    website: string;
+}
+
+interface UserDetailProps{
+    user: User;
+}
+
+export default function DetailUser(props: UserDetailProps) {
     const router = useRouter();
-    const { id } = router.query;
+    const { user } = props;
 
     return (
-        <div>
-            <p>
-                Detail User
-                {' '}
-                { id }
-                {' '}
-            </p>
-        </div>
+        <Layout pageTitle="Detail User">
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.phone}</p>
+            <p>{user.website}</p>
+            <button type="button" onClick={() => { router.push('/users'); }}>Back</button>
+        </Layout>
     );
+}
+
+export async function getStaticPaths() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUsers = await response.json();
+
+    const paths = dataUsers.map((user: User) => ({
+        params: {
+            id: `${user.id}`,
+        },
+    })); // mereturn ke paths dg isi list params id dr api
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+interface GetStaticProps{
+    params: {
+        id: string;
+    }
+}
+
+export async function getStaticProps(context: GetStaticProps) {
+    const { id } = context.params;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const user = await response.json();
+
+    return {
+        props: {
+            user,
+        },
+    };
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import styles from '../../styles/Users.module.css';
 
 interface UsersProps{
     dataUsers: Array<any>;
@@ -8,15 +12,16 @@ interface UsersProps{
 
 export default function Users(props: UsersProps) {
     const { dataUsers } = props;
-    console.log(dataUsers);
+    const router = useRouter();
+    // console.log(dataUsers);
 
     return (
         <Layout pageTitle="Users">
             {dataUsers.map((user) => (
-                <>
-                    <p className={user.id}>{user.name}</p>
-                    <p className={user.id}>{user.email}</p>
-                </>
+                <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={styles.card}>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                </div>
             ))}
         </Layout>
     );
