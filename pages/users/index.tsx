d87158d4df6465b15f46500381cebfdc77dba22c
luthfyhakim <luthfyhakim250404@gmail.com>
2022-05-21 09:16:47 +0700

    data fetching dgn getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -2,12 +2,34 @@
 /* eslint-disable import/no-unresolved */
 import Layout from '../../components/Layout';
 
-export default function Users() {
+interface UsersProps{
+    dataUsers: Array<any>;
+}
+
+export default function Users(props: UsersProps) {
+    const { dataUsers } = props;
+    console.log(dataUsers);
+
     return (
         <Layout pageTitle="Users">
-            <div>
-                <p>Index</p>
-            </div>
+            {dataUsers.map((user) => (
+                <>
+                    <p className={user.id}>{user.name}</p>
+                    <p className={user.id}>{user.email}</p>
+                </>
+            ))}
         </Layout>
     );
+}
+
+// data fetching API
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUsers = await response.json();
+
+    return {
+        props: {
+            dataUsers,
+        },
+    };
 }
