83b61251c99a1ca723e62bbf158688441c823db0
luthfyhakim <luthfyhakim250404@gmail.com>
2022-05-23 08:11:47 +0700

    data fetching getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,13 +1,48 @@
+/* eslint-disable max-len */
+/* eslint-disable semi */
+/* eslint-disable array-callback-return */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+import {
+    Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal,
+} from 'react';
 import Layout from '../components/Layout';
+import styles from '../styles/Blog.module.css';
 
-export default function Blog() {
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    map: any;
+    blog: any;
+}
+
+interface BlogPosts {
+    dataBlog: Post;
+}
+
+export default function Blog(props: BlogPosts) {
+    const { dataBlog } = props;
+
     return (
         <Layout pageTitle="Blog">
-            <div>
-                <p>Page Blog</p>
-            </div>
+            {dataBlog.map((blog: { id: Key | null | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; body: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => (
+                <div key={blog.id} className={styles.card}>
+                    <h3>{blog.title}</h3>
+                    <p>{blog.body}</p>
+                </div>
+            ))}
         </Layout>
     );
+}
+
+export async function getServerSideProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const dataBlog = await response.json();
+
+    return {
+        props: {
+            dataBlog,
+        },
+    };
 }
